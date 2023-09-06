a0368f93dc548a66dbddea9dbe75307b0d480f3a
AigerimAdr <aigerim210596@gmail.com>
2023-09-06 15:19:50 +0600

    Переписала код на Redux Toolkit

---
+++ src/components/Cart.jsx
@@ -0,0 +1,41 @@
+import { useSelector, useDispatch } from "react-redux";
+import { cartActions } from "../store/Cartslice";
+
+function Cart() {
+  const cartItems = useSelector((state) => state.cart.items);
+  const dispatch = useDispatch();
+
+  const handleRemoveFromCart = (itemId) => {
+    dispatch(cartActions.removeFromCart(itemId));
+  };
+
+  const handleClearCart = () => {
+    dispatch(cartActions.clearCart());
+  };
+
+  return (
+    <div className="cart">
+      <h2>Корзина</h2>
+      <div className="cart-list">
+        {cartItems.map((item) => (
+          <div className="cart-item" key={item._id}>
+            <h3>{item.name}</h3>
+            <p>Цена: {item.price}</p>
+            <button onClick={() => handleRemoveFromCart(item._id)}>
+              Удалить
+            </button>
+          </div>
+        ))}
+      </div>
+      {cartItems.length > 0 && (
+        <div className="cart-controls">
+          <button className="btn-basket" onClick={handleClearCart}>
+            Очистить корзину
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Cart;
